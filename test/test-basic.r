322f4144648e6964dd9e5d6d4e83c8dc871117aa
Bruno Vilela <bvilela@boteromacserver2.wustl.edu>
2017-07-13 12:40:17 -0500

    add tab tests

--- test/test-basic.r
+++ test/test-basic.r
@@ -13,4 +13,17 @@ test_that("can connect to app", {
   expect_equal(appTitle, "Wallace")  
 })
 
+test_that("Tabs are present", {  
+  webElems <- remDr$findElements("id", "tabs")
+  appCtrlLabels <- sapply(webElems, function(x){x$getElementText()})
+  appCtrlLabels <- unlist(strsplit(appCtrlLabels[[1]], "\n"))
+  expect_equal(appCtrlLabels[1], "Intro")  
+  expect_equal(appCtrlLabels[2], "1 Occ Data")  
+  expect_equal(appCtrlLabels[3], "2 Process Occs")  
+  expect_equal(appCtrlLabels[4], "3 Env Data")  
+  expect_equal(appCtrlLabels[5], "4 Process Envs")  
+  expect_equal(appCtrlLabels[6], "5 Partition Occs")  
+  expect_equal(appCtrlLabels[7], "6 Model")  
+  expect_equal(appCtrlLabels[8], "Session Code")  
+})
 remDr$close()
